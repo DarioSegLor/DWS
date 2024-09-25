54728c0da6f87157b67732438b7d8c5c7cee896c
DarioSegLor <dario.seg.lor@gmail.com>
2024-09-25 13:48:47 +0200

    Fixed Class paths

---
+++ 1rEV/T15/examples/BookArchitecture/src/main/java/com/example/BookArchitecture/BookArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookArchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookArchitectureApplication.class, args);
+	}
+
+}
